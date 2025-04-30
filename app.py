ee1e4000c8466f31bcc5122b1b1fe7668409d44e
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-04-30 10:11:51 +0330

    tutorial

---
+++ app.py
@@ -0,0 +1,10 @@
+
+course_name = "Python Programming"
+
+print(len(course_name))
+print(course_name[0])
+print(course_name[-1])
+print(course_name[0:3])
+print(course_name[0:])
+print(course_name[:3])
+print(course_name[:])
