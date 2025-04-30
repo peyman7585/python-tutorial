ce034258859a7bf391dae3aa068e4fd78ab2de88
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-04-30 12:07:33 +0330

    syntax and output

--- app.py
+++ app.py
@@ -8,3 +8,16 @@ print(course_name[0:3])
 print(course_name[0:])
 print(course_name[:3])
 print(course_name[:])
+
+Name = input("What is your name?!")
+
+print("Hello," + Name + "! Welcome to python programming")
+
+greeting = "Hello"
+subject = "World"
+
+message = greeting + "," + subject + "!"
+message2 = f"Hello , {subject}"
+print(message)
+print(message2)
+
