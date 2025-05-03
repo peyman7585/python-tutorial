171365fd4a4ad3c7b865562a0d3ab74c3eb4ed4b
peyman Abbaszadeh <abbaszadeh.8575@gmail.com>
2025-05-03 12:21:14 +0330

    Basic Python Syntax

--- app.py
+++ app.py
@@ -1,23 +1,100 @@
 
 course_name = "Python Programming"
 
-print(len(course_name))
-print(course_name[0])
-print(course_name[-1])
-print(course_name[0:3])
-print(course_name[0:])
-print(course_name[:3])
-print(course_name[:])
+# print(len(course_name))
+# print(course_name[0])
+# print(course_name[-1])
+# print(course_name[0:3])
+# print(course_name[0:])
+# print(course_name[:3])
+# print(course_name[:])
 
-Name = input("What is your name?!")
+# Name = input("What is your name?!")
 
-print("Hello," + Name + "! Welcome to python programming")
+#print("Hello," + Name + "! Welcome to python programming")
 
 greeting = "Hello"
 subject = "World"
 
 message = greeting + "," + subject + "!"
 message2 = f"Hello , {subject}"
-print(message)
-print(message2)
+# print(message)
+# print(message2)
 
+is_student = True
+# print("is the person a student?", is_student)
+
+a = 10
+b = 4
+sum_result = a + b
+division_result = a / b
+
+# print("Sum: ", sum_result)
+# print("Division: ", division_result)
+
+
+Greeting = "Hello"
+Name = "Mobin"
+Welcome = Greeting + ", " + Name + "!"
+# print(Welcome)
+
+
+# is_sun = True
+# has_umberlla = False
+
+# if is_sun and not has_umberlla:
+#     print("Enjoy the sunshine!")
+# else:
+#     print("Better take an umberlla")
+
+# number = input("Enter a number: ")
+# number = int(number)
+# print("your number multiplied by 2 is :" + str(number * 2))
+
+# name = "Bob"
+# age = 25
+# print("Name: " + name + ", Age:" + str(age))
+# print(f"Name:{name} , Age:{age}")
+# print("Name: {} , Age: {}".format(name,age))
+
+# ######
+
+# a = 20
+# b = 13
+
+# addition = a + b
+# subtraction = a - b
+# multiplication = a * b
+# division = a / b
+# modulus = a % b
+
+# print(addition)
+# print(subtraction)
+# print(multiplication)
+# print(division)
+# print(modulus)
+
+# greater_than = a > b
+# less_than = a < b
+# equal_to = a == b
+# not_equal = a != b
+
+# print(greater_than)
+# print(less_than)
+# print(equal_to)
+# print(not_equal)
+
+# a = True
+# b = False
+
+# logical_and = a and b
+# logical_or = a or b
+# logical_not_a = not a
+# logical_not_b = not b
+
+x = -15
+y = 5
+
+difference = abs(x - y)
+
+print("The absolute difference between ", x, "and", y, "is", difference )
